5655c9ab6e7812617919399a49b70594b8f6245a
Kai-Barker <241065@virtualwindow.co.za>
2025-05-27 00:48:15 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,7 @@ app.use(cors({
   credentials: true
 }));
 //Middleware to parse JSON bodies
-<<<<<<< Updated upstream
-app.use(express.json({limit:'1mb'}));
-=======
-app.use(express.json());
+app.use(express.json({limit: '1mb'}));
 
 // Use Express session
 app.use(session({
@@ -39,7 +36,6 @@ app.use(session({
 // app.use((req,res,next) => {
 //     console.log(req.session);
 // });
->>>>>>> Stashed changes
 
 //MONGODB connection 
 app.get('/', (req, res) => {
